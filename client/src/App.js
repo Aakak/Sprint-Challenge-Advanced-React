89415d19c490fca22837902ce15c4625eac78b5a
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-04 08:40:14 -0700

    Added and Displayed Players Data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
+import PlayersData from "./Components/PlayersData";
+import Navbar from "./Components/Navbar";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      PlayersData: []
+    };
+    console.log("Constructor is running!");
+  }
+
+
+componentDidMount () {
+  axios 
+  .get("http://localhost:5000/api/players?per_page=7")
+  .then(res => {
+    this.setState({
+      PlayersData: res.data,
+      // FollowersCards: this.state.FollowersCards
+    });
+  })
+  .catch(err => console.log(err));
+  console.log("Axios is running")
 }
 
+render () {
+  console.log("rendering");
+  return (
+    <div className="App">
+     <Navbar />
+     <h1 className="main-h1">Women's World Cup players</h1>
+     <h4 className="main-h4">Google Search Trends June-July 2019</h4>
+    {this.state.PlayersData.map(function(player) { 
+      console.log(player.name);
+       return <PlayersData { ...player } />
+      }
+    )} 
+    </div>
+  )
+}
+}
 export default App;
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useDarkMode } from '../Hooks/useDarkMode';
+
+
+const Navbar = () => {
+  const [darkMode, toggleDarkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    toggleDarkMode();
+  };
+  
+  return (
+    <nav className="navbar">
+      <h1 className="nav-h1">World Cup</h1>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
